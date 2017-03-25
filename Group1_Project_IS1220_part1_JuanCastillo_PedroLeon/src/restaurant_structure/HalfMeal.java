848c42fbd102537b7f451e453549369d1413a8c4
pleonpita <pleonpita@gmail.com>
2017-03-25 14:26:59 +0100

    res s 4

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/restaurant_structure/HalfMeal.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/restaurant_structure/HalfMeal.java
@@ -1,28 +1,11 @@
 package restaurant_structure;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HalfMeal extends Meal {
 	
 	public HalfMeal(String name, List<Item> mealItems){
 		super(name, mealItems);
-	}
-	
-	public static void main (String[] args){
-
-		Starter s = new Starter("s", 2, "Veg");
-		MainDish md = new MainDish("md", 5, "Veg");
-		
-		List<Item> items = new ArrayList<Item>();
-		
-		items.add(s);
-		items.add(md);
-		
-		Meal m = new HalfMeal("mediterraneo", items);
-		
-		System.out.println(m.getType());
-		
 	}
 
 }
--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/restaurant_structure/Menu.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/restaurant_structure/Menu.java
@@ -12,13 +12,51 @@ import java.util.ArrayList;
  */
 public class Menu {
 	
-	List<Item> items = new ArrayList<Item>();
+	List<Starter> starters = new ArrayList<Starter>();
+	List<MainDish> mainDishes= new ArrayList<MainDish>();
+	List<Dessert> desserts = new ArrayList<Dessert>();
+	
+	public Menu(List<Starter> starters, List<MainDish> mainDishes, List<Dessert> desserts){
+		this.starters = starters;
+		this.mainDishes = mainDishes;
+		this.desserts = desserts;
+	}
+	
+	public void addStarter(Starter s){
+		starters.add(s);
+	}
+	
+	public void removeStarter(Starter s){
+		starters.remove(s);
+	}
+	
+	public void addMainDish(MainDish md){
+		mainDishes.add(md);
+	}
+	
+	public void removeMainDish(MainDish md){
+		mainDishes.remove(md);
+	}
+	
+	public void addDessert(Dessert d){
+		desserts.add(d);
+	}
+	
+	public void removeDessert(Dessert d){
+		desserts.remove(d);
+	}
 	
 	@Override
 	public String toString(){
 		String res = "";
-		for(Item i : items){
-			res = res + (i.toString());
+		for(Starter s : starters){
+			res = res + (s.toString());
+		}
+		for(MainDish md : mainDishes){
+			res = res + (md.toString());
+		}
+		for(Dessert d : desserts){
+			res = res + (d.toString());
 		}
 		return res;
 	}
