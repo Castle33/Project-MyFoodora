5cd898aee561f07372310119dff929de279410c7
Castle33 <jcastillorodriguez33@gmail.com>
2017-03-26 18:47:54 +0200

    Add Customer

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/users/Customer.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/users/Customer.java
@@ -1,11 +1,17 @@
 package users;
 
+import system.FidelityCard;
+import system.FidelityCardBasic;
+import system.FidelityCardPoint;
+import system.FidelityCardLottery;
+
 public class Customer extends User{
 	
 	private String surname;
 	private Address address;
 	private String email;
 	private String phoneNumber;
+	private FidelityCard fidelityCard;
 	
 	public Customer(String name, String username, String surname, Address address, String email, 
 			String phoneNumber, String password) {
@@ -14,6 +20,7 @@ public class Customer extends User{
 		this.address = address;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
+		this.fidelityCard = new FidelityCardBasic();
 	}
 	
 	@Override
@@ -21,6 +28,51 @@ public class Customer extends User{
 		return "Customers [name=" + getName() + "surname=" + surname + ", username=" + getUsername() + 
 				", ID=" + getID() + ", address=" + address + ", email=" + email + ", phoneNumber="
 				+ phoneNumber + "]";
+	}
+	
+	/***************************************************************************************************/
+	/*
+	 * Fidelity Card: methods to get and set Fidelity Card Plan and change points of FidelityCardPoint
+	 */
+	
+	public FidelityCard getFidelityCard() {
+		return fidelityCard;
+	}
+
+	public void setFidelityCard(FidelityCard fidelityCard) {
+		if(!(fidelityCard.getClass().equals(this.fidelityCard.getClass()))){
+			this.fidelityCard = fidelityCard;
+		}	
+	}
+	
+	public void setFidelityCardtoBasic(){
+		FidelityCardBasic basic = new FidelityCardBasic();
+		setFidelityCard(basic);
+	}
+	
+	public void setFidelityCardtoPoints(){
+		FidelityCardPoint points = new FidelityCardPoint();
+		setFidelityCard(points);
+	}
+	
+	public void setFidelityCardtoLottery(){
+		FidelityCardLottery lottery = new FidelityCardLottery();
+		setFidelityCard(lottery);
+	}
+	
+	public int getNumberFidelityPoints(){
+		if(this.fidelityCard instanceof FidelityCardPoint){
+			return ((FidelityCardPoint) this.fidelityCard).getPoints();
+		}
+		return 0;
+	}
+	
+	public void addFidelityPoints(int n){
+		if(this.fidelityCard instanceof FidelityCardPoint){
+			((FidelityCardPoint) this.fidelityCard).setPoints(((FidelityCardPoint) this.fidelityCard).getPoints()+n);
+		} else {
+			System.out.println("Fidelity Card is not of type Points");
+		}
 	}
 	
 	/***************************************************************************************************/
