362be0f242afbe422663a3bbb4110bb788c5f99e
pleonpita <pleonpita@gmail.com>
2017-04-02 12:03:30 +0200

    core Test

---
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/tests/CoreTest.java
@@ -0,0 +1,204 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoreTest {
+
+	@Test
+	public void testCore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserLogIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterObserver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveObserver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyObservers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActivateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeactivateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeServiceFee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeMarkup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeDeliveryCost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTargetProfitToDeliveryCost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTargetProfitToMarkup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTargetProfitToServiceFee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParameterToTargetProfit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeTotalIncome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeTotalProfit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeAverageIncome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLeastActiveRestaurant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMostActiveRestaurant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLeastActiveCourier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMostActiveCourier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDeliveryToFastest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDeliveryToFairOccupation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayRestaurantInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSpecialMeal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveSpecialMeal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlaceNewOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterFidelityCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnregisterFidelityCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHistoryOrders() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGiveRemoveConsensus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnregisterCourier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCourierState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCourierPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessOrders() {
+		fail("Not yet implemented");
+	}
+
+}
