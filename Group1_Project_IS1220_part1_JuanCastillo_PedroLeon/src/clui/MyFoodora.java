9000dabcf68480f8bf158fe0671644713d40933a
Castle33 <jcastillorodriguez33@gmail.com>
2017-04-03 12:28:09 +0200

    MyFoodora v3

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/clui/MyFoodora.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/clui/MyFoodora.java
@@ -2,15 +2,22 @@ package clui;
 
 import system.*;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+
 import restaurant_structure.Meal;
 
 public class MyFoodora {
-	protected static Core core = new Core();
+	protected static Core core;
 	protected static StringCast stringCast;
+	protected static ArrayList<Meal> listTempMeals;
 	private String name;
 	private ArrayList<String> args;
-	protected static ArrayList<Meal> listTempMeals;
+	private static ArrayList<String> listCmds;
 	
 	
 	public MyFoodora() {
@@ -18,6 +25,8 @@ public class MyFoodora {
 		this.name = "";
 		this.args = new ArrayList<String>();
 		MyFoodora.listTempMeals = new ArrayList<Meal>();
+		MyFoodora.core = new Core();
+		MyFoodora.readCmdFile("listCmd.txt");
 	}
 
 	public void launcherCL() {
@@ -32,6 +41,8 @@ public class MyFoodora {
 			while(!input.equals("stop")){
 				input = sc.nextLine();
 				cmdNameOutput = readCmdNameArgs(input);
+				System.out.println(cmdNameOutput);
+				
 				System.out.print(">");
 			}
 			System.out.println("Leaving MyFoodora\tSee you later ;)");
@@ -48,7 +59,7 @@ public class MyFoodora {
 			//TO-DO : display list of commands
 		} else {
 			String[] input = s.trim().split(" ");
-			if(!input[0]){ //TO-DO boolean function that checks if name is in the list of Commands
+			if(!checkCmdExists(input[0])){ //TO-DO boolean function that checks if name is in the list of Commands
 				name = "";
 				return "Error finding command: Make sure the command is correctly introduced";
 			} else {
@@ -75,7 +86,11 @@ public class MyFoodora {
 	}
 	
 	public boolean checkCmdExists(String name){
-		
+		if(listCmds.contains(name)){
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public void processCommand(String name, ArrayList<String> args){
@@ -84,11 +99,54 @@ public class MyFoodora {
 		}
 	}
 	
+	public static void readCmdFile(String fileName) {
+		  
+		  String stringCmds = "";
+		  FileReader file = null;
+		  BufferedReader reader = null;
+		  
+		  try {
+			  // open input stream pointing at fileName
+			  file = new FileReader(fileName);
+			  
+			  // open input buffered reader to read file line by line
+			  reader = new BufferedReader(file);
+			  String line = "";
+			  
+			  // reading input file line by line
+			  while ((line = reader.readLine()) != null) {
+				  stringCmds += line + " ";
+			  }
+		  } catch (Exception e) {
+		      throw new RuntimeException(e);
+		  } finally {
+		    if (file != null) {
+		      try {
+		        file.close();
+		        reader.close();
+		       
+		      } catch (IOException e) {
+		    	  System.out.println("File not found: " + fileName);
+		        // Ignore issues during closing 
+		      }
+		    }
+		  }
+		  String[] tempListCmd = stringCmds.split(" ");
+		  listCmds = arrayToList(tempListCmd);
+		} 
 	
-	public String[] listToArray(ArrayList<String> args) {
+	public static String[] listToArray(ArrayList<String> args) {
 		String[] array = new String[args.size()];
 		for(int i=0; i<array.length; i++){
 			array[i] = args.get(i);
+		}
+		return array;
+	}
+	
+	public static ArrayList<String> arrayToList(String[] args) {
+		ArrayList<String> array = new ArrayList<String>();
+		for(int i=0; i<args.length; i++){
+			array.add(args[i]) ;
 		}
 		return array;
 	}
