fe580c3b2a3b215f25c4936f0d2bb8d904592b4d
pleonpita <pleonpita@gmail.com>
2017-04-03 09:21:30 +0200

    CommandProcessor and reader

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/clui/testReadFrom.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/clui/testReadFrom.java
@@ -1,9 +1,20 @@
 package clui;
 
+import java.util.Scanner;
+
 public class testReadFrom {
 	
 	public static void main (String[] args){
-		CommandLine.getInstance();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¡Welcome to MyFoodora!\tCheck possible commands = type 'help'\tEnd program = type 'stop'");
+		System.out.print(">");
+		String message = sc.nextLine();
+		while(!message.equals("stop")){
+			System.out.println(message);
+			System.out.print(">");
+			message = sc.nextLine();
+		}
+		System.out.println("Leaving MyFoodora\tSee you later ;)");
 	}
 
 }
