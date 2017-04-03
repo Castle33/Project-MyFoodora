6e73b8ac43a6cb68d743a4682e097e87d78bc934
Castle33 <jcastillorodriguez33@gmail.com>
2017-04-03 10:14:13 +0200

    MyFoodora update

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/clui/MyFoodora.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/clui/MyFoodora.java
@@ -1,6 +1,43 @@
 package clui;
+
 import system.*;
+import java.util.Scanner;
 
 public class MyFoodora {
 	protected static Core core = new Core();
+	private String name;
+	private String[] args;
+	
+	public void launcherCL() {
+		Scanner sc;
+		String input = "";
+		try {
+			sc = new Scanner(System.in);
+			System.out.println("¡Welcome to MyFoodora!\tCheck possible commands = type 'help'\tEnd program = type 'stop'");
+			System.out.print(">");
+			
+			while(!input.equals("stop")){
+				input = sc.nextLine();
+				
+				System.out.print(">");
+			}
+			System.out.println("Leaving MyFoodora\tSee you later ;)");
+			sc.close();
+			
+		} catch (Exception e) {
+			System.out.println("Unexpected error: Please try again in a few minutes");
+		}
+	}
+	
+	public void readCmdName(String s) {
+		
+		if(s.equals("help")){
+			//TO-DO : display list of commands
+		} else {
+			String[] input = s.trim().split(" ");
+			//Split arguments by ""
+			
+			
+		}
+	}
 }
