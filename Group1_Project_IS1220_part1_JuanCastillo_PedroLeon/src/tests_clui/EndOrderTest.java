77416ed9c2cf6c9a41faebcdb34c62f5e713b3c9
pleonpita <pleonpita@gmail.com>
2017-04-09 23:07:52 +0200

    EndOrder test

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/tests_clui/EndOrderTest.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/tests_clui/EndOrderTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import clui.EndOrder;
 import clui.MyFoodora;
+import users.User;
+import users.Courier;
 
 public class EndOrderTest {
 
@@ -13,17 +15,34 @@ public class EndOrderTest {
 	public void testProcess() {
 		MyFoodora mf = new MyFoodora();
 		String[] args1 = new String[1];
-		args1[0] = "Chinois";
+		args1[0] = "Chinois1";
+		String[] args2 = new String[1];
+		args2[0] = "Chinois2";
+		String[] args3 = new String[1];
+		args3[0] = "Chinois3";
 		EndOrder eo = new EndOrder();
-		mf.treatCmd("registerRestaurant \"Bonheur\" \"1,4\" \"Emporter\" \"ilfautfermer\"");
-		mf.treatCmd("registerCustomer \"Luis\" \"Cobas\" \"mocho\" \"6,6\" \"fallingInMIX\" \"0695599143\" \"luchocobas@gmail.com\"");
+		mf.treatCmd("registerCourier \"Angel\" \"Antolin\" \"PotaBestia\" \"0,0\" \"cricri\" \"0695599143\"");
+		mf.treatCmd("registerCourier \"Yisus\" \"Martinez\" \"Tulipanes\" \"9,0\" \"cenicienta\" \"0695599143\"");
+		mf.treatCmd("registerRestaurant \"Bonheur\" \"4,3\" \"Emporter\" \"ilfautfermer\"");
+		mf.treatCmd("registerRestaurant \"Dominos\" \"0,5\" \"pizza\" \"barbecueistop\"");
+		mf.treatCmd("registerCustomer \"Luis\" \"Cobas\" \"mocho\" \"0,3\" \"fallingInMIX\" \"0695599143\" \"luchocobas@gmail.com\"");
+		mf.treatCmd("registerCustomer \"Marc\" \"Bataillou\" \"rata\" \"5,5\" \"tofeliz\" \"0695599143\" \"bataillou@gmail.com\"");
+		mf.treatCmd("registerCustomer \"Emilio\" \"Emilio\" \"emilio\" \"6,0\" \"rae\" \"0695599143\" \"emilioemilio@gmail.com\"");
+		
 		mf.treatCmd("logIn \"mocho\" \"fallingInMIX\"");
 		System.out.println(eo.process(args1));
-		assertTrue(eo.process(args1).equals("Order: -Chinois- not created in MyFoodora.\nFor order creation use createOrder <restaurantUsername> <orderName>."));
-		mf.treatCmd("createOrder \"Emporter\" \"Chinois\"");
+		assertTrue(eo.process(args1).equals("Order: -Chinois1- not created in MyFoodora.\nFor order creation use createOrder <restaurantUsername> <orderName>."));
+		mf.treatCmd("createOrder \"Emporter\" \"Chinois1\"");
 		System.out.println(eo.process(args1));
-		assertTrue(eo.process(args1).equals("Order: -Chinois- has nor meals nor items."));
+		assertTrue(eo.process(args1).equals("Order: -Chinois1- has nor meals nor items."));
 		mf.treatCmd("logOut \"\"");
+		mf.treatCmd("logIn \"rata\" \"tofeliz\"");
+		mf.treatCmd("createOrder \"pizza\" \"Chinois2\"");
+		mf.treatCmd("logOut \"\"");
+		mf.treatCmd("logIn \"emilio\" \"rae\"");
+		mf.treatCmd("createOrder \"Emporter\" \"Chinois3\"");
+		mf.treatCmd("logOut \"\"");
+		
 		mf.treatCmd("logIn \"Emporter\" \"ilfautfermer\"");
 		mf.treatCmd("createMeal \"Vegie\" \"fullmeal\"");
 		mf.treatCmd("addDishRestaurantMenu \"Dumplings\" \"starter\" \"glutenfree\" \"4.8\"");
@@ -34,10 +53,31 @@ public class EndOrderTest {
 		mf.treatCmd("addDish2Meal \"Douhua\" \"Vegie\"");
 		mf.treatCmd("saveMeal \"Vegie\"");
 		mf.treatCmd("logOut \"\"");
+		
+		mf.treatCmd("logIn \"pizza\" \"barbecueistop\"");
+		mf.treatCmd("addDishRestaurantMenu \"Dumplings\" \"starter\" \"glutenfree\" \"4.8\"");
+		mf.treatCmd("logOut \"\"");
+		
 		mf.treatCmd("logIn \"mocho\" \"fallingInMIX\"");
-		mf.treatCmd("addItem2Order \"Chinois\" \"Dumplings\" \"3\"");
-		mf.treatCmd("addMeal2Order \"Chinois\" \"Vegie\" \"3\"");
-		assertTrue(eo.process(args1).equals("Customer: -mocho- placed Order: -Chinois- to Restaurant: -Bonheur-."));
+		mf.treatCmd("addItem2Order \"Chinois1\" \"Noodles\" \"1\"");
+		mf.treatCmd("addMeal2Order \"Chinois1\" \"Vegie\" \"1\"");
+		assertTrue(eo.process(args1).equals("Customer: -mocho- placed Order: -Chinois1- to Restaurant: -Bonheur-."));
+		mf.treatCmd("logOut \"\"");
+		mf.treatCmd("logIn \"rata\" \"tofeliz\"");
+		mf.treatCmd("addItem2Order \"Chinois2\" \"Dumplings\" \"2\"");
+		assertTrue(eo.process(args2).equals("Customer: -rata- placed Order: -Chinois2- to Restaurant: -Dominos-."));
+		mf.treatCmd("logOut \"\"");
+		mf.treatCmd("logIn \"mocho\" \"rae\"");
+		mf.treatCmd("addMeal2Order \"Chinois3\" \"Vegie\" \"3\"");
+		assertTrue(eo.process(args3).equals("Customer: -emilio- placed Order: -Chinois3- to Restaurant: -Bonheur-."));
+		mf.treatCmd("logOut \"\"");
+		for(User u : MyFoodora.getCore().getListOfUsers().values()){
+			if(u instanceof Courier){
+				System.out.println(u.getUsername());
+				System.out.println(((Courier) u).getCurrentOrder());
+				System.out.println(((Courier) u).getListPendingOrders());
+			}
+		}
 	}
 
 }
