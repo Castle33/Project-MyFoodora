842b94fe8f4ee187da117bb29fe85d5dea6861c4
Castle33 <jcastillorodriguez33@gmail.com>
2017-04-30 14:14:28 +0200

    Correction ShowCourier

--- Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/tests_clui/ShowCouriersRegisteredTest.java
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/tests_clui/ShowCouriersRegisteredTest.java
@@ -15,9 +15,10 @@ public class ShowCouriersRegisteredTest {
 		String[] args1 = new String [1];
 		args1[0] = "";
 		ShowCouriersRegistered scr = new ShowCouriersRegistered();
+		mf.treatCmd("logIn \"deputy\" \"987654321\"");
 		mf.treatCmd("registerCourier \"Mr\" \"Amazon\" \"courier1\" \"2,1\" \"pass1\" \"0695436143\"");
 		mf.treatCmd("registerCourier \"Ms\" \"Fedex\" \"courier2\" \"4,7\" \"pass2\" \"0616299143\"");
-		mf.treatCmd("logIn \"deputy\" \"987654321\"");
+		
 		System.out.println(scr.process(args1));
 	}
 
