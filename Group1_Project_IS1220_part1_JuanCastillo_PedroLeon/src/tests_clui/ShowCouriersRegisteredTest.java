6ae82d4ce291574745bc76e6b138b0359c4deac0
Castle33 <jcastillorodriguez33@gmail.com>
2017-04-30 13:40:20 +0200

    ShowCouriersRegisteredTest

---
+++ Group1_Project_IS1220_part1_JuanCastillo_PedroLeon/src/tests_clui/ShowCouriersRegisteredTest.java
@@ -0,0 +1,24 @@
+package tests_clui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import clui.MyFoodora;
+import clui.ShowCouriersRegistered;
+
+public class ShowCouriersRegisteredTest {
+
+	@Test
+	public void test() {
+		MyFoodora mf = new MyFoodora();
+		String[] args1 = new String [1];
+		args1[0] = "";
+		ShowCouriersRegistered scr = new ShowCouriersRegistered();
+		mf.treatCmd("registerCourier \"Mr\" \"Amazon\" \"courier1\" \"2,1\" \"pass1\" \"0695436143\"");
+		mf.treatCmd("registerCourier \"Ms\" \"Fedex\" \"courier2\" \"4,7\" \"pass2\" \"0616299143\"");
+		mf.treatCmd("logIn \"deputy\" \"987654321\"");
+		System.out.println(scr.process(args1));
+	}
+
+}
